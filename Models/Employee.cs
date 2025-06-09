9cee9a5356dd7e791d0cf82f1bd18e28e9ebf837
priyo <mithun21@gmail.com>
2025-06-09 13:19:07 +1000

    Added post methods and delete calls

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,15 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
 namespace CrudAPITests.Models;
 
 public class Employee
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string DateOfBirth { get; set; }
-    public string StartDate { get; set; }
-    public string Department { get; set; }
-    public string JobTitle { get; set; }
-    public string Email { get; set; }
-    public string Mobile { get; set; }
-    public string Address { get; set; }
-    public string BaseSalary { get; set; }
+    [JsonProperty("_id", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public string? EmployeeId { get; set; }
+    [Required]
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public string DateOfBirth { get; set; } = String.Empty;
+    public string? StartDate { get; set; }
+    public string? Department { get; set; }
+    public string? JobTitle { get; set; }
+    public string? Email { get; set; }
+    public string? Mobile { get; set; }
+    public string? Address { get; set; }
+    public string? BaseSalary { get; set; }
 }
--- utils/ApiHelper.cs
+++ utils/ApiHelper.cs
@@ -2,36 +2,39 @@ using System.Net;
 using Newtonsoft.Json;
 using RestSharp;
 
-namespace DefaultNamespace;
+namespace CrudAPITests.utils;
 
 public static class ApiHelper
 {
-    private static string baseUrl;
+    private static string _baseUrl = String.Empty;
     static void SetupBaseUrl()
     {
-        var fileName = Path.Join(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName,"keyLookUp.txt");
+        var fileName = Path.Join(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName, "keyLookUp.txt");
         if (!File.Exists(fileName))
             throw new FileNotFoundException();
-        baseUrl = File.ReadAllText(fileName);
+        _baseUrl = File.ReadAllText(fileName);
     }
 
     public static async Task<T> GetRestResponse<T>(string url)
     {
         SetupBaseUrl();
-        var options = new RestClientOptions(baseUrl) {
+        var options = new RestClientOptions(_baseUrl) {
         };
         var client = new RestClient(options);
         var request = new RestRequest(url);
         var response = await client.ExecuteGetAsync(request);
         if (response.StatusCode is HttpStatusCode.BadRequest || response.Content is null)
             throw new HttpRequestException();
-        return JsonConvert.DeserializeObject<T>(response.Content);
+        var result = JsonConvert.DeserializeObject<T>(response.Content);
+        if (result == null)
+            throw new InvalidOperationException("Failed to deserialize the response content.");
+        return result;
     }
 
     public static async Task<RestResponse> PostRestResponse(string url, string body)
     {
         SetupBaseUrl();
-        var options = new RestClientOptions(baseUrl) {
+        var options = new RestClientOptions(_baseUrl) {
         };
         var client = new RestClient(options);
         var request = new RestRequest(url, Method.Post);
@@ -43,7 +46,7 @@ public static class ApiHelper
     public static async Task<RestResponse> DeleteRestResponse(string url)
     {
         SetupBaseUrl();
-        var options = new RestClientOptions(baseUrl) {
+        var options = new RestClientOptions(_baseUrl) {
         };
         var client = new RestClient(options);
         var request = new RestRequest(url);
